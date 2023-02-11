e09b0b3d79568d0e018360b0b09ed2e1e83675f4
Alberto-1992 <beto_1866@outlook.com>
2023-02-11 02:12:59 -0600

    cambios agregados a los estilos de las vistas

--- modals/cargarPacienteCE.php
+++ modals/cargarPacienteCE.php
@@ -2,7 +2,7 @@
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-       
+
     <script src="js/getCatalogos.js"></script>
     <script src="js/scriptModalvalidacionCE.js"></script>
     <script src="https://cdn.rawgit.com/igorescobar/jQuery-Mask-Plugin/1ef022ab/dist/jquery.mask.min.js"></script>
