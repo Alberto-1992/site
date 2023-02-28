96ae69682505a8d4b07e7030021c01428d097a40
Alberto Vazquez <beto_1866@outlook.com>
2023-02-28 07:57:07 -0600

    cambio en artritis

--- frontend/artritisvista.php
+++ frontend/artritisvista.php
@@ -88,7 +88,7 @@
     ?>
     <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'>
     </script>
-    <script src="js/multiple-select-cancermama.js"></script>
+    
 </body>
 
 </html>
